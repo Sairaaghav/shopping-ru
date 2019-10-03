5bfdd34fa1b8b9ef23cd6c0d039766548dc80c06
Sairaaghav <raaghav23@gmail.com>
2019-10-03 10:39:22 +0530

    Add files via upload

---
+++ src/main/java/org/sample/shopping_ru/catalogue/CatalogueItem.java
@@ -0,0 +1,29 @@
+package org.sample.shopping_ru.catalogue;
+
+import java.math.BigDecimal;
+
+public class CatalogueItem {
+
+	String sku;
+	String name;
+	BigDecimal unitPrice;
+	
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public BigDecimal getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(BigDecimal unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+}
